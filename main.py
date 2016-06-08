ba2ba8f3b62e8b11a72c65a8321e35b80140de62
Roman Zayrullin <krosenmann@gmail.com>
2016-06-08 14:16:06 +0300

    exported

---
+++ main.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pygame as pg
+from game-classes import *
+from functions import *
+from player import *
+from datetime import datetime
+import numpy as np
