1003ed6b641296e93361cbada217061052cfa8e5
Roman Zayrullin <krosenmann@gmail.com>
2016-06-08 18:30:56 +0300

    Export new changed file

--- display.py
+++ display.py
@@ -1,13 +1,21 @@
 
 import pygame
-from pygame.locals import *
-from OpenGL.GL import *
-from OpenGL.GLU import *
+#from pygame.locals import *
+#from OpenGL.GL import *
+#from OpenGL.GLU import *
+from ConfigParser import SafeConfigParser
 
-import surface
+parser = SafeConfigParser()
+try:
+    parser.read('gameconfig.cfg')
+    resolution = parser.get('display','resolution')
+except:
+    print TypeError
+    resolution = (640, 480)
+#import surface
 
 class Display:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Far Prototype")
-        self.set_display((640, 480))
+        self.set_display(resolution)
