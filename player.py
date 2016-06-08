21580e10ab2648f4d16b416323ba7face458b3df
Roman Zayrullin <krosenmann@gmail.com>
2016-06-08 14:17:03 +0300

    exported

---
+++ player.py
@@ -0,0 +1,12 @@
+
+# -*- coding: utf-8 -*-
+from game-classes import *
+from functions import *
+
+name = 'None'
+def createHero():
+  
+  return playerStat
+   
+ 
+player = Сharacter(name, playerStat, playerPerks)
