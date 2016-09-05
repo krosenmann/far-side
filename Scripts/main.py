4afe8d35f6da0d8118bf3b8141559b9ba7239850
velocifero <altvelociphero@gmail.com>
2016-09-05 05:37:39 +0300

    Добавил скрипт создания окна, какой-никакой конфиг, его парсинг

--- Scripts/main.py
+++ Scripts/main.py
@@ -2,21 +2,66 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 import sys
+import configparser
+
+# Read game.cfg
+config = configparser.RawConfigParser()
+config.read("game.cfg")
+window = int(config.get("VIDEO", "Window"))
 
 
-def draw():
-    return 0
+def init(width, height):
+    glClearColor(0.0, 0.0, 0.0, 0.5)
+    glClearDepth(1.0)
+    glDepthFunc(GL_LESS)
+    glEnable(GL_DEPTH_TEST)
+    glShadeModel(GL_SMOOTH)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45.0, float(config.get("VIDEO", "Width")) / float(config.get("VIDEO", "Height")), 0.1, 100.0)
+    glMatrixMode(GL_MODELVIEW)
 
-glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
 
-glutInitWindowSize(800, 600)
+def resize_window(width, height):
+    if height == 0:
+        height = 1
 
-glutInitWindowPosition(150, 150)
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45.0, float(width) / float(height), 0.1, 100.0)
+    glMatrixMode(GL_MODELVIEW)
 
-glutInit(sys.argv)
 
-glutCreateWindow(b"FAR SIDE")
+def draw_scene():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    glutSwapBuffers()
 
-glutDisplayFunc(draw)
 
-glutMainLoop()
+def exit_game(key):
+    if key == GLUT_KEY_UP:  # Press W to Exit
+        glutDestroyWindow(window)
+        sys.exit()
+
+
+def main():
+    global window
+    glutInit('')
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
+    glutInitWindowSize(int(config.get("VIDEO", "Width")), int(config.get("VIDEO", "Height")))
+    glutInitWindowPosition(int(config.get("VIDEO", "WindowPositionX")), int(config.get("VIDEO", "WindowPositionY")))
+    window = glutCreateWindow(b"FAR SIDE")
+    glutDisplayFunc(draw_scene)
+    # if bool(config.get("VIDEO", "WindowedMode")):
+    #    glutFullScreen()
+    # else:
+    #    print("WindowedMode")
+    glutIdleFunc(draw_scene)
+    glutReshapeFunc(resize_window)
+    glutKeyboardFunc(exit_game)
+    init(int(config.get("VIDEO", "Width")), int(config.get("VIDEO", "Height")))
+    glutMainLoop()
+
+
+main()
