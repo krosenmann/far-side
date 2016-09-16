82c93bc641f415f5e5129f72e3c34bb09e25a12f
Roman Zayrullin <krosenmann@gmail.com>
2016-09-17 01:27:25 +0300

    Добавил скелет интерпретатора команд

---
+++ Scripts/comand-interpreter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def 
+
+def commandConsole():
+    """Интерфейс ввода команд"""
+    while True:
+        pass
+
+if __name__ === 'main':
+    comandConsole()
